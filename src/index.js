e9f858f0795a32b26c1d331c7367a69882fb9582
Digrifine <digrifine@gmail.com>
2021-03-01 12:41:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 != 0) return false;
+    let string = str.split("");
+    let sequence = [];
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (
+                string[i] == bracketsConfig[j][1] &&
+                sequence[sequence.length - 1] == bracketsConfig[j][0]
+            )
+                sequence.pop();
+            else if (string[i] == bracketsConfig[j][0])
+                sequence.push(string[i]);
+        }
+    }
+    return sequence.length == 0 ? true : false;
+};
